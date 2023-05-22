07d460e8335a5c88519d86b7f75e30e910788be6
Wulidiya <125673894+Wulidiya@users.noreply.github.com>
2023-05-22 09:54:08 +0700

    praktikum

---
+++ 22_Middleware/Praktikum/struktur_projek/middlewares/jwtMiddlewares.go
@@ -0,0 +1,29 @@
+package middlewares
+
+import (
+	"struktur_projek/constants"
+	"time"
+
+	jwt "github.com/dgrijalva/jwt-go"
+	"github.com/labstack/echo"
+)
+
+func CreateToken(userId int) (string, error) {
+	claims := jwt.MapClaims{}
+	claims["authorized"] = true
+	claims["userId"] = userId
+	claims["exp"] = time.Now().Add(time.Hour * 1).Unix()
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
+	byteSecret := []byte(constants.SECRET_JWT)
+	return token.SignedString(byteSecret)
+}
+
+func ExtractTokenUserId(e echo.Context) int {
+	user := e.Get("user").(*jwt.Token)
+	if user.Valid {
+		claims := user.Claims.(jwt.MapClaims)
+		userId := claims["userId"].(int)
+		return userId
+	}
+	return 0
+}
